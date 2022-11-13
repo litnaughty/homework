cfa528cac1466839b393bfea178fd4561a9e16ea
litnaughty <117559961+litnaughty@users.noreply.github.com>
2022-11-13 16:19:42 +0800

    Add files via upload

---
+++ text.cpp
@@ -0,0 +1,489 @@
+#undef QQQdialect 
+#undef QQQ146
+#undef QQQ148
+#undef QQQbndchk
+/* Note: for BITMAP execution histories QQQthreaded is not defined */
+#undef QQQthreaded
+#define QQQdo178b
+#undef QQQfixbra
+#undef QQQswitch3
+#undef QQQternary
+#define QQQswitch
+#define qqqMaxBranchDepth      2
+static int zzfileid =      8;
+#undef QQQFORKFL
+#undef QQQIADDRF
+#undef QQQADRMOD
+#undef QQQDCAPFL
+#undef QQQCOMPRESSED_EXH
+#define QQQMAINFL
+#undef QQQSINGLEFILE
+#undef QQQFILEID
+#undef QQQseparate
+static int ytprogramzzopen = 0;
+#ifndef FILE
+#include <stdio.h>
+/* If stdio.h is not available then insert:   typedef int * FILE;
+#endif
+*/
+#endif
+#undef QQ_ANSI_PROTOTYPE   
+#ifdef __STDC__    
+#define QQ_ANSI_PROTOTYPE 1 
+#else      
+#ifdef VAXC  
+#define QQ_ANSI_PROTOTYPE 1    
+#endif   
+#endif 
+#define QQQnumfil 40  /* default setting */   
+#define LDRA_DUBLIN    
+	static int diagopen = 0; 
+	#define qqzzidfield 3   
+	static int ytprogramzqqzqz(); 
+	static int ytprogramzqqzqz1(); 
+	static int ytprogramzzqqzz (int qqqi);   
+	static int ytprogramzqzqzq (int qqqi);  
+	static int ytprogramzqfqzz (int qqqi);   
+	static int ytprogramzzzqtz (int qqqa,int qqqb); 
+	static int ytprogramzzzqfz (int qqqa,int qqqb); 
+	static int ytprogramzzdo17(int a,int b);   
+	static int ytprogramzzqqzzFULine (int nLine);   
+static int ytprogramzzqqzzFU0(int nLine1,int Result,int nLine);   
+/*------------- END OF PLTEST PROTOTYPES ------------------------------*/ 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+int main(){ 
+
+int yutianzfp1=ytprogramzzqqzz( 6);int n, x, i, x1, x2, x3 ;
+printf( "请输入年级和需要的题目数量");
+scanf( "%d %d",& n,& x);
+srand(( unsigned int)
+time( NULL));
+ytprogramzzqqzz( 11);switch 
+(
+n 
+)
+{ 
+case 1:
+{ 
+int yutianzfp2=ytprogramzzqqzz( 18);for 
+(
+
+
+
+(i=0)
+; 
+
+
+
+(i<x)
+; 
+
+
+
+
+(i++)
+)
+{ 
+int yutianzfp3=ytprogramzzqqzz( 34);x1=
+rand()% 10+ 1 ;
+x2=
+rand()% 10 ;
+ytprogramzzqqzz( 38);if 
+
+
+(rand()%2==1){ 
+int yutianzfp4=ytprogramzzqqzz( 44);printf( "%d+%d=\n", x1, x2);
+ytprogramzzqqzz( 45);} 
+else 
+{ 
+int yutianzfp5=ytprogramzzqqzz( 48);printf( "%d-%d=\n", x1, x2);
+ytprogramzzqqzz( 49);} 
+ytprogramzzqqzz( 50);} 
+ytprogramzzqqzz( 51);break ;
+ytprogramzzqqzz( 52);} 
+case 2:
+{ 
+int yutianzfp6=ytprogramzzqqzz( 55);for 
+(
+
+
+
+(i=0)
+; 
+
+
+
+(i<x)
+; 
+
+
+
+
+(i++)
+)
+{ 
+int yutianzfp7=ytprogramzzqqzz( 71);x1=
+rand()% 50+ 1 ;
+x2=
+rand()% 50+ 1 ;
+x3=
+rand()% 50+ 1 ;
+ytprogramzzqqzz( 77);if 
+
+
+(rand()%2==1){ 
+int yutianzfp8=ytprogramzzqqzz( 83);if 
+
+
+(ytprogramzzdo17 (
+ytprogramzzzqtz(
+(x1+x2-x3)<50?ytprogramzzdo17 (1,100009):ytprogramzzdo17 (0,100009),100006)
+||
+ytprogramzzdo17 (
+(x1-x2+x3)<50?1:0,100012)
+?1:0,100015)
+){ 
+int yutianzfp9=ytprogramzzqqzz( 89);printf( "%d+%d-%d=\n", x1, x2, x3);
+ytprogramzzqqzz( 90);} 
+else 
+{ 
+int yutianzfp10=ytprogramzzqqzz( 93);continue ;
+ytprogramzzqqzz( 94);} 
+ytprogramzzqqzz( 95);} 
+else 
+{ 
+int yutianzfp11=ytprogramzzqqzz( 98);if 
+
+
+(rand()%2==0){ 
+int yutianzfp12=ytprogramzzqqzz( 104);if 
+
+
+(ytprogramzzdo17 (
+ytprogramzzzqtz(
+(x1+x2-x3)<50?ytprogramzzdo17 (1,100023):ytprogramzzdo17 (0,100023),100020)
+||
+ytprogramzzdo17 (
+(x1-x2+x3)<50?1:0,100026)
+?1:0,100029)
+){ 
+int yutianzfp13=ytprogramzzqqzz( 110);printf( "%d-%d+%d=\n", x1, x2, x3);
+ytprogramzzqqzz( 111);} 
+else 
+{ 
+int yutianzfp14=ytprogramzzqqzz( 114);continue ;
+ytprogramzzqqzz( 115);} 
+ytprogramzzqqzz( 116);} 
+ytprogramzzqqzz( 117);} 
+ytprogramzzqqzz( 118);} 
+ytprogramzzqqzz( 119);break ;
+ytprogramzzqqzz( 120);} 
+case 3:
+{ 
+int yutianzfp15=ytprogramzzqqzz( 123);for 
+(
+
+
+
+(i=0)
+; 
+
+
+
+(i<x)
+; 
+
+
+
+
+(i++)
+)
+{ 
+int yutianzfp16=ytprogramzzqqzz( 139);x1=
+rand()% 1000+ 1 ;
+x2=
+rand()% 1000+ 1 ;
+x3=
+rand()% 1000+ 1 ;
+ytprogramzzqqzz( 145);if 
+
+
+(rand()%2==1){ 
+int yutianzfp17=ytprogramzzqqzz( 151);printf( "%d+%d*%d=\n", x1, x2, x3);
+ytprogramzzqqzz( 152);} 
+else 
+{ 
+int yutianzfp18=ytprogramzzqqzz( 155);printf( "%d-%d*%d=\n", x1, x2, x3);
+ytprogramzzqqzz( 156);} 
+ytprogramzzqqzz( 157);} 
+ytprogramzzqqzz( 158);break ;
+ytprogramzzqqzz( 159);} 
+case 4:
+{ 
+int yutianzfp19=ytprogramzzqqzz( 162);for 
+(
+
+
+
+(i=0)
+; 
+
+
+
+(i<x)
+; 
+
+
+
+
+(i++)
+)
+{ 
+int yutianzfp20=ytprogramzzqqzz( 178);x1=
+rand();
+x2=
+rand();
+x3=
+rand();
+ytprogramzzqqzz( 184);if 
+
+
+(rand()%2==1){ 
+int yutianzfp21=ytprogramzzqqzz( 190);if 
+
+
+(rand()%2==1){ 
+int yutianzfp22=ytprogramzzqqzz( 196);printf( "(%d+%d)*%d=\n", x1, x2, x3);
+ytprogramzzqqzz( 197);} 
+else 
+{ 
+int yutianzfp23=ytprogramzzqqzz( 200);printf( "(%d-%d)*%d=\n", x1, x2, x3);
+ytprogramzzqqzz( 201);} 
+ytprogramzzqqzz( 202);} 
+else 
+{ 
+int yutianzfp24=ytprogramzzqqzz( 205);if 
+
+
+(rand()%2==1){ 
+int yutianzfp25=ytprogramzzqqzz( 211);printf( "%d-(%d*%d)=\n", x1, x2, x3);
+ytprogramzzqqzz( 212);} 
+else 
+{ 
+int yutianzfp26=ytprogramzzqqzz( 215);printf( "%d+(%d*%d)=\n", x1, x2, x3);
+ytprogramzzqqzz( 216);} 
+ytprogramzzqqzz( 217);} 
+ytprogramzzqqzz( 218);} 
+ytprogramzzqqzz( 219);break ;
+ytprogramzzqqzz( 220);} 
+default:
+{ 
+int yutianzfp27=ytprogramzzqqzz( 223);printf( "\nerror");
+ytprogramzzqqzz( 224);} 
+} 
+ytprogramzzqqzz( 226);return 0
+; 
+ytprogramzzqqzz( 227);} 
+
+#define qqqbranches    277
+#define QQQMAXMCDCSIZE    698
+FILE * ytprogramzzhfil;  
+static char qqqqone[]   = "%6d\n";           /* single point */     
+static char qqqqtwo[]   = "%6d%6d\n";        /* double point or single + rentrant */  
+static char qqqqthree[] = "%6d%6d%6d\n";     /* sp + re + module  or dp + re      */        
+static char qqqqfour[]  = "%6d%6d%6d%6d\n";  /* double point + re + module         */    
+/* ----------------------------------------------------------------------   
+*  
+* For ANSI or Kernighan and Richie parameter lists    
+* define QQ_ANSI_PROTOTYPE  
+*      
+* ----------------------------------------------------------------------    
+*/          
+#undef QQ_ANSI_PROTOTYPE   
+#ifdef __STDC__    
+#define QQ_ANSI_PROTOTYPE 1   
+#else    
+#ifdef VAXC  
+#define QQ_ANSI_PROTOTYPE 1  
+#endif /* VAXC */
+#endif /* __STDC__ */  
+#undef ldra_fprintf  
+#undef ldra_sscanf  
+/*  #define ldra_fprintf */    
+/*  #define ldra_sscanf */   
+/* static int ytprogramzzopen = 0; is used to indicate if file is open      */   
+#define QQBUFFERlimitQQ 2000 /* length of buffer for buffered output  */
+static void ytqqoutput(FILE * f,char * s,int i);  
+static void ytqqoutput(FILE * f,char * s,int i)  
+{    
+    fprintf(f,s,i); 
+} /* endof ytqqoutput */ 
+static void ytqqoutput0(FILE * f,char * s); 
+static void ytqqoutput0(FILE * f,char * s)  
+{  
+        fprintf(f,s);       
+} /* endof ytqqoutput0 */     
+static void ytqqoutput2(FILE * f,char * s,int i, int j); 
+static void ytqqoutput2(FILE * f,char * s,int i, int j)  
+{         
+        fprintf(f,s,i,j);   
+} /* endof ytqqoutput2 */   
+static void ytqqoutput3(FILE * f,char * s,int i,int j,int k);  
+static void ytqqoutput3(FILE * f,char * s,int i,int j,int k)  
+{    
+        fprintf(f,s,i,j,k); 
+} /* endof ytqqoutput3 */
+static void ytqqoutput4(FILE * f,char * s,int i,int j,int k,int l);
+static void ytqqoutput4(FILE * f,char * s,int i,int j,int k,int l)  
+{    
+        fprintf(f,s,i,j,k,l);    
+} /* endof ytqqoutput4 */ 
+static int ytprogramzqqzqz();   
+static int ytprogramzqqzqz()  
+{        
+ if( ytprogramzzopen != 1 ){  
+ /* -------------------    
+   if( ytprogramzzopen == 0 ) {   
+    ytprogramzzhfil =        
+       fopen( "1-2.exh","w");    
+  }          
+  else {     
+    ytprogramzzhfil =  
+       fopen( "1-2.exh","a");    
+  }         
+ ---------------------*/      
+ ytprogramzzopen = 1;   
+ }  
+return(1);  
+}       
+static int ytprogramzqqzqz1();   
+static int ytprogramzqqzqz1()  
+ {      
+  if( ytprogramzzopen != 1 ) {  
+ if( ytprogramzzopen == 0 ) { 
+    ytprogramzzhfil =  
+       fopen( "1-2.exh","w");    
+    }            
+  else {  
+     ytprogramzzhfil = 
+       fopen( "1-2.exh","a");    
+  }             
+   ytprogramzzopen = 1; 
+}       
+ return(1);  
+}         
+static int ytprogramzzqqzz (int qqqi); 
+static int ytprogramzzqqzz     
+(int qqqi)   
+{         
+  if( ytprogramzzopen != 1 ) {    
+       ytprogramzqqzqz1();   
+ };           
+ if( ytprogramzzopen ) {      
+   /* uncompressed */   
+  /* uncompressed */ 
+   /* unbuffered */  
+   ytqqoutput(ytprogramzzhfil,   
+   qqqqone,qqqi); 
+ }       
+ return(1); 
+}       
+static int ytprogramzqzqzq (int qqqi);   
+static int ytprogramzqzqzq     
+(int qqqi)   
+{        
+{     
+if( ytprogramzzopen != 1 ) {  
+ ytprogramzqqzqz1();    
+ };       
+ /* notcompressed notbitmap */  
+ /* notcompressed notbitmap notbuffered */    
+    
+/* upload the history */    
+/* uncompressed */   
+fclose(ytprogramzzhfil); 
+ytprogramzzopen = 2;  
+/*      
+* #ifdef QQQbndchk    
+*   if( qzqzqbnd == 0 ) {   
+*     bound_output("No Bounds exceeded");    
+*   }         
+*   else {    
+*     bound_output("Bounds exceeded");   
+*   }     
+* #endif  
+*/     
+ }    
+return(1);     
+} /* endof zqzqzq */    
+static int ytprogramzqfqzz (int qqqi);  
+static int ytprogramzqfqzz 
+ (int qqqi)      
+{       
+ {     
+ int qqql;     
+  qqql=qqqi-1;  
+  ytprogramzzqqzz(qqql);  
+  ytprogramzzqqzz(qqqi); 
+ }           
+ return (1);  
+} /* endof zqfqzz */     
+static int ytprogramzzzqtz (int qqqa,int qqqb); 
+static int ytprogramzzzqtz 
+ (int qqqa,int qqqb)     
+{          
+ {          
+   if(qqqa){               
+     ytprogramzzqqzz(qqqb) ;       
+   };    
+ }   
+ return(qqqa);   
+} /* endof zzzqtz */      
+static int ytprogramzzzqfz (int qqqa,int qqqb); 
+static int ytprogramzzzqfz   
+  (int qqqa,int qqqb) 
+{      
+ {    
+   if(qqqa) { 
+    return(qqqa);}      
+   else {       
+     ytprogramzzqqzz(qqqb);    
+  }      
+ }    
+ return(qqqa);  
+} /* zzzqfz */  
+static int ytprogramzzdo17(   
+    int a,int b)  
+{       
+  if( a ){   
+    ytprogramzzqqzz(b-1);    
+  }          
+  else {    
+   ytprogramzzqqzz(b);  
+ };     
+ return a; 
+} /*  endof zzdo17 */     
+static int ytprogramzzqqzzFU0(int nLine1,int Result,int nLine)
+{         
+ return(Result); 
+}  /*  endof zzFU     0*/
+static int ytprogramzzqqzzFULine (int nLine); 
+static int ytprogramzzqqzzFULine     
+(int nLine)   
+{         
+  if( ytprogramzzopen != 1 ) {    
+       ytprogramzqqzqz1();   
+ };           
+ if( ytprogramzzopen ) {      
+   /* uncompressed */   
+  /* uncompressed */ 
+   /* unbuffered */  
+   ytqqoutput(ytprogramzzhfil,   
+   qqqqone,nLine+90000); 
+ }       
+ return(nLine+90000); 
+}  /*  endof FULine */     
+/* END OF PLTEST INSTRUMENTATION */   
